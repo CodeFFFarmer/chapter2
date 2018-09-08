2da6fd66809117b0bb0aa0d1205daac7942d628f
DESKTOP-3VOK9K2\Spark <1085580730@qq.com>
2018-09-08 17:26:11 +0800

    增加方法

--- src/main/java/com.zhanglu.chapter2/util/PropsUtil.java
+++ src/main/java/com.zhanglu.chapter2/util/PropsUtil.java
@@ -40,4 +40,60 @@ public class PropsUtil {
         }
         return props;
     }
+
+    /**
+     * 获取字符型属性（默认值为空字符串）
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    public static String getString(Properties props, String key, String defaultValue) {
+        return getString(props, key, "");
+    }
+
+    /**
+     * 获取字符型属性（可指定默认值）
+     */
+    public static String getString(Properties props, String key, String defaultValue) {
+        String value = defaultValue;
+        if (props.containsKey(key)) {
+            value = props.getProperty(key);
+        }
+        return value;
+    }
+
+    /**
+     * 获取数值型属性（默认值为 0）
+     */
+    public static int getInt(Properties props, String key) {
+        return getInt(props, key, 0);
+    }
+
+    // 获取数值型属性（可指定默认值）
+    public static int getInt(Properties props, String key, int defaultValue) {
+        int value = defaultValue;
+        if (props.containsKey(key)) {
+            value = CastUtil.castInt(props.getProperty(key));
+        }
+        return value;
+    }
+
+    /**
+     * 获取布尔型属性（默认值为 false）
+     */
+    public static boolean getBoolean(Properties props, String key) {
+        return getBoolean(props, key, false);
+    }
+
+    /**
+     * 获取布尔型属性（可指定默认值）
+     */
+    public static boolean getBoolean(Properties props, String key, boolean defaultValue) {
+        boolean value = defaultValue;
+        if (props.containsKey(key)) {
+            value = CastUtil.castBoolean(props.getProperty(key));
+        }
+        return value;
+    }
 }
