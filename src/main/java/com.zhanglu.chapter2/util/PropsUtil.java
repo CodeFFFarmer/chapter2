4b79622d353e78cc3c5a7a88dae6122a810d1f42
DESKTOP-3VOK9K2\Spark <1085580730@qq.com>
2018-09-08 13:37:42 +0800

    属性文件工具类

--- src/main/java/com.zhanglu.chapter2/util/PropsUtil.java
+++ src/main/java/com.zhanglu.chapter2/util/PropsUtil.java
@@ -1,4 +1,29 @@
 package com.zhanglu.chapter2.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PropsUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
+    /**
+     * 加载属性文件
+     * @param filename
+     * @return
+     */
+    public static Properties loadProps(String filename) {
+        Properties props = null;
+        InputStream is = null;
+        try {
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+        } catch () {
+
+        } finally {
+
+        }
+    }
 }
