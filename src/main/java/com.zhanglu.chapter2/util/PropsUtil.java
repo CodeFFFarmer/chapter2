1e76353dbb5a99fdc2bbc820f401f65d2a65ffb3
ZhangL <1323753919@qq.com>
2018-09-09 12:05:27 +0800

    增加CastUtil,StringUtil

--- src/main/java/com.zhanglu.chapter2/util/PropsUtil.java
+++ src/main/java/com.zhanglu.chapter2/util/PropsUtil.java
@@ -45,10 +45,9 @@ public class PropsUtil {
      * 获取字符型属性（默认值为空字符串）
      * @param props
      * @param key
-     * @param defaultValue
      * @return
      */
-    public static String getString(Properties props, String key, String defaultValue) {
+    public static String getString(Properties props, String key) {
         return getString(props, key, "");
     }
 
@@ -65,12 +64,21 @@ public class PropsUtil {
 
     /**
      * 获取数值型属性（默认值为 0）
+     * @param props
+     * @param key
+     * @return
      */
     public static int getInt(Properties props, String key) {
         return getInt(props, key, 0);
     }
 
-    // 获取数值型属性（可指定默认值）
+    /**
+     * 获取数值型属性（可指定默认值）
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
+     */
     public static int getInt(Properties props, String key, int defaultValue) {
         int value = defaultValue;
         if (props.containsKey(key)) {
@@ -81,6 +89,9 @@ public class PropsUtil {
 
     /**
      * 获取布尔型属性（默认值为 false）
+     * @param props
+     * @param key
+     * @return
      */
     public static boolean getBoolean(Properties props, String key) {
         return getBoolean(props, key, false);
@@ -88,6 +99,10 @@ public class PropsUtil {
 
     /**
      * 获取布尔型属性（可指定默认值）
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
      */
     public static boolean getBoolean(Properties props, String key, boolean defaultValue) {
         boolean value = defaultValue;
